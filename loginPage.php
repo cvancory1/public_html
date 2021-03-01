69bb5bef639c2d27acdfe7b47f1e9e79b5359da3
cvancory1 <cvancory1@gmail.com>
2021-03-01 17:43:42 -0500

    same

--- loginPage.php
+++ loginPage.php
@@ -20,7 +20,6 @@
 	<p> Password </p>
 	<input type="password" id="password" name = "password" style = "border: 2px solid black; padding: 0.5rem; border-radius: 4px; background-color: #e6e6ff;">
 	
-	
     <input type="submit"  class = "button"/> 
 
 </body> 

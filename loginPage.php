75572f1a85e499310343bd79f654a0b5d61fb5f8
cvancory1 <cvancory1@gmail.com>
2021-03-01 17:38:27 -0500

    push

---
+++ loginPage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> 
+<html lang = "en"> 
+    <header> 
+
+        <title> Test - Alumni Database Title</title> 
+
+    </header>
+
+
+<body> 
+
+  <div class="main">
+	<h3>Login</h3>
+	
+    <form name="add_name" id="add_name" action="loginCheck.php" method="post">  
+
+	<p> Username </p>
+	<input type="text" id="username" name = "username"> 
+	
+	<p> Password </p>
+	<input type="password" id="password" name = "password" style = "border: 2px solid black; padding: 0.5rem; border-radius: 4px; background-color: #e6e6ff;">
+	
+	
+    <input type="submit"  class = "button"/> 
+
+</body> 
+
+</html>

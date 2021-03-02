dddaeda166a797655248a1164b7f6b1dcc53974f
cvancory1 <cvancory1@gmail.com>
2021-03-02 10:50:12 -0500

    same

--- loginPage.php
+++ loginPage.php
@@ -2,30 +2,37 @@
 
 <!DOCTYPE html> 
 <html lang = "en"> 
-<link rel="stylesheet" href="dat.css" type="text/css" />
+    <link rel="stylesheet" href="dat.css" type="text/css" />
 
-    <header> 
+    <div class= "margin">
 
-        <title> Test - Alumni Database Title</title> 
-
-    </header>
-<body> 
+     <p class="topBar"> Alumni Database </p>
 
 
-    <div class="LoginMain">
-        <h3>Login</h3>
-        
-        <form name="loginForm" id="loginForm" action="loginCheck.php" method="post">  
-
-        <p> Username </p>
-        <input type="text" id="username" name = "username"> 
-        
-        <p> Password </p>
-        <input type="password" id="password" name = "password" style = "border: 2px solid black; padding: 0.5rem; border-radius: 4px; background-color: #e6e6ff;">
-        
-        <br /><input type="submit"  class = "loginButton"/> 
     </div>
 
-</body> 
+    <body> 
 
-</html>
+        <div class="loginMain">
+            <p class="loginBox" id="login"> Login </p>
+            
+            <form name="add_name" id="add_name" action="loginCheck.php" method="post">  
+    
+                <p class="loginBox" id="username"> Username </p>
+            <input type="text" id="username"  name = "username"> 
+    
+    
+            <p class="loginBox" id="password"> Password </p>
+            <input type="password" id="password"  name = "password">
+            
+            <br /> <input type="submit"  class = "button"/> 
+    
+        </div>
+    
+    
+    </body> 
+
+
+
+</html> 
+

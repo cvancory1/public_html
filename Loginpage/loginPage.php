721db47e090f09af1ab06418059ea3807375b59d
cvancory1 <cvancory1@gmail.com>
2021-03-07 15:07:53 -0500

    test

---
+++ Loginpage/loginPage.php
@@ -0,0 +1,39 @@
+<!-- this was originallly from glasfish DB and i altered it... have to change back or replace -->
+
+<!DOCTYPE html> 
+<html lang = "en"> 
+    <link rel="stylesheet" href="login.css" type="text/css" />
+
+    <body> 
+
+
+        <div class="loginMain">
+             <p class="topBar" id="top_header"> Alumni Database </p>
+
+            <p class="topBar" id="login"> Login </p>
+            
+            <form name="add_name" id="add_name" action="loginCheck.php" method="post" ">  
+                
+
+                <input type="text" id="username"  name = "username" placeholder="Username"> 
+        
+                <br>
+                <input type="password" id="password"  name = "password" placeholder="Password">
+                
+                <br /> <input type="submit"  class = "button"/> 
+
+                <br><br><br> <p>
+                            <a href =https://lamp.salisbury.edu/~cvancory1/creatAccount.php >Create Account </a>
+                            <br>  
+                            <a href =https://lamp.salisbury.edu/~cvancory1/forgotPassword.php >Forgot Password</a>
+
+                    </p> 
+    
+        </div>
+    
+    
+    </body> 
+
+
+</html> 
+

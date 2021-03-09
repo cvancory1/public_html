780a5fc4e2d8084b135ea5dcb17b7750883c86e2
cvancory1 <cvancory1@gmail.com>
2021-03-08 20:38:08 -0500

    testing functionality on the server

--- Homepage/UITest.php
+++ Homepage/UITest.php
@@ -221,7 +221,7 @@
 
             function closeNav() {
                 document.getElementById("mySidebar").style.width = "0";
-                document.getElementById("main").style.marginLeft= "10";
+                document.getElementById("main").style.marginLeft= "0";
 
             }
         </script>

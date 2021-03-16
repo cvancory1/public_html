67f08aefd9d4db12cff13d49c8d56c39147f530e
cvancory1 <cvancory1@gmail.com>
2021-03-15 20:33:37 -0400

    testing functionality on the server

--- Loginpage/loginCheck.php
+++ Loginpage/loginCheck.php
@@ -42,6 +42,8 @@ if (isset($_POST['username']) and isset($_POST['password'])) {
 
                // since password is already verfied and username exists
                $sql = "select admin from Login where username='$username'";
+               echo $sql;
+
               $row=mysqli_fetch_array($r);
                echo $row;
 
@@ -59,7 +61,7 @@ if (isset($_POST['username']) and isset($_POST['password'])) {
 
 
                // }
-               // print_r($_SESSION);
+               print_r($_SESSION);
 
                header( "refresh:1;url=https://lamp.salisbury.edu/~cvancory1/Homepage/UITest.php" );
 

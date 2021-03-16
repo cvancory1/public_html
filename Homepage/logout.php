0234b99e7d3c203c2be79f19989957bb54d2504f
cvancory1 <cvancory1@gmail.com>
2021-03-15 22:23:58 -0400

    testing functionality on the server

--- Homepage/logout.php
+++ Homepage/logout.php
@@ -1,5 +1,5 @@
-?php
+<?php
     session_start();
     session_destroy();
-    header( "refresh:1;url=https://lamp.salisbury.edu/~cvancory1/Homepage/loginP.html" );
+    header( "refresh:1;url=https://lamp.salisbury.edu/~cvancory1/Homepage/loginP.html");
 ?>

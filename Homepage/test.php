bf08b6c239b81975d043a8d17b7ae9b53ebeb90f
cvancory1 <cvancory1@gmail.com>
2021-03-25 21:53:09 -0400

    testing functionality on the server

--- Homepage/test.php
+++ Homepage/test.php
@@ -5,28 +5,30 @@
     session_start();
     // echo $_SESSION["privilege"];
     print_r($_SESSION['privilege']);
+    print_r($_SESSION["privilege"]);
+    echo"<p> here</p>";
+
+    if ($_SESSION["privilege"] == "superUser") {
+        
+        echo "<div class='crud >
+        <a href='animalsRemove.php'>Delete</a>
+        <a href='animalsUpdate1.php'>Update</a>
+        <a href='animalsAdd.php'>Add</a>
+        </div>";
+    }else if ($_SESSION["privilege"] == "editUser"){
+        echo "editUser";
+
+    }else if ($_SESSION["privilege"] == "viewUser"){
+        echo "viewUser";
+
+    }
 
 
 ?>
 
 <p> hello</p>
         <!-- the operations for superUser and Edit Users -->
-        <?php   
-        if ($_SESSION["privilege"] == "superUser") {
-        
-            echo "<div class='crud >
-            <a href='animalsRemove.php'>Delete</a>
-            <a href='animalsUpdate1.php'>Update</a>
-            <a href='animalsAdd.php'>Add</a>
-            </div>";
-        }else if ($_SESSION["privilege"] == "editUser"){
-            echo "editUser";
-
-        }else if ($_SESSION["privilege"] == "viewUser"){
-            echo "viewUser";
-
-        }
-        ?>
+     
  <div class="profile">
             <!-- <button class="button">  </button> -->
              <a href='logout.php'>Log Out</a>

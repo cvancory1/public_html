b8dd127245410a9f96daa830cd785c93118184e5
cvancory1 <cvancory1@gmail.com>
2021-03-29 00:35:48 -0400

    testing functionality on the server

--- Homepage/test.php
+++ Homepage/test.php
@@ -41,7 +41,6 @@ if (isset($_SESSION['LAST_ACTIVITY']) &&
     $message = " Timing out";
     echo "<script type='text/javascript'>alert('$message');</script>";
     echo "<script> test();</script>";
-    echo"hi";
     session_unset();
     session_destroy();
     session_start();

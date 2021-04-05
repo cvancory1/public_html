9260d1d39321be2f7964f84befb1dacaf23c9dbe
cvancory1 <cvancory1@gmail.com>
2021-04-05 15:58:24 -0400

    testing functionality on the server

--- Homepage/test.php
+++ Homepage/test.php
@@ -92,36 +92,36 @@ $_SESSION['LAST_ACTIVITY'] = $time;
             }
             echo "<div id='mainTable'>";
 
-            echo "
-                <div id= 'mySidebar' class= 'sidebar'>
-                <a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>×</a>
-                <a href='#'>Address</a>
-                <a href='#'>Alumni</a>
-                <a href='#'>Donated</a>
-                <a href='#'>Donation</a>
-                <a href='#'>Employer</a>
-                <a href='#'>Majored In</a>
-                <a href='#'>Minored In</a>
-                <a href='#'>Program</a>
-                <a href='#'>School</a>
-                <a href='#'>Works At</a>
-                </div>
-            
-                <div class='menu'>
-                <button class='openbtn' onclick='openNav()'>☰</button>
-                <button class='placeholder'>X</button> 
-                <button class='placeholder'>X</button>
-                <button class='placeholder'>X</button>
-                <button class='placeholder'>X</button>
-                </div>
+                echo "
+                    <div id= 'mySidebar' class= 'sidebar'>
+                    <a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>×</a>
+                    <a href='#'>Address</a>
+                    <a href='#'>Alumni</a>
+                    <a href='#'>Donated</a>
+                    <a href='#'>Donation</a>
+                    <a href='#'>Employer</a>
+                    <a href='#'>Majored In</a>
+                    <a href='#'>Minored In</a>
+                    <a href='#'>Program</a>
+                    <a href='#'>School</a>
+                    <a href='#'>Works At</a>
+                    </div>
+                
+                    <div class='menu'>
+                    <button class='openbtn' onclick='openNav()'>☰</button>
+                    <button class='placeholder'>X</button> 
+                    <button class='placeholder'>X</button>
+                    <button class='placeholder'>X</button>
+                    <button class='placeholder'>X</button>
+                    </div>
 
 
-                <br> <br> <br> <br>
-            
-            
-            ";
+                    <br> <br> <br> <br>
+                
+                
+                ";
 
-            include 'AlumniTable.php';
+                include 'AlumniTable.php';
             echo "</div>"; // end of mainTable 
 
 

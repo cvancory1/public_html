550c35eea23a28b7b5a53fc9c5c3063fb50bf105
cvancory1 <cvancory1@gmail.com>
2021-04-05 18:07:06 -0400

    testing functionality on the server

--- Homepage/test.php
+++ Homepage/test.php
@@ -88,34 +88,34 @@ $_SESSION['LAST_ACTIVITY'] = $time;
                 </div>";
             }
 
-                echo "
-                    <div id= 'mySidebar' class= 'sidebar'>
-                    <a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>×</a>
-                    <a href='#'>Address</a>
-                    <a href='#'>Alumni</a>
-                    <a href='#'>Donated</a>
-                    <a href='#'>Donation</a>
-                    <a href='#'>Employer</a>
-                    <a href='#'>Majored In</a>
-                    <a href='#'>Minored In</a>
-                    <a href='#'>Program</a>
-                    <a href='#'>School</a>
-                    <a href='#'>Works At</a>
-                    </div>
+                // echo "
+                //     <div id= 'mySidebar' class= 'sidebar'>
+                //     <a href='javascript:void(0)' class='closebtn' onclick='closeNav()'>×</a>
+                //     <a href='#'>Address</a>
+                //     <a href='#'>Alumni</a>
+                //     <a href='#'>Donated</a>
+                //     <a href='#'>Donation</a>
+                //     <a href='#'>Employer</a>
+                //     <a href='#'>Majored In</a>
+                //     <a href='#'>Minored In</a>
+                //     <a href='#'>Program</a>
+                //     <a href='#'>School</a>
+                //     <a href='#'>Works At</a>
+                //     </div>
                 
-                    <div class='menu'>
-                    <button class='openbtn' onclick='openNav()'>☰</button>
-                    <button class='placeholder'>X</button> 
-                    <button class='placeholder'>X</button>
-                    <button class='placeholder'>X</button>
-                    <button class='placeholder'>X</button>
-                    </div>
+                //     <div class='menu'>
+                //     <button class='openbtn' onclick='openNav()'>☰</button>
+                //     <button class='placeholder'>X</button> 
+                //     <button class='placeholder'>X</button>
+                //     <button class='placeholder'>X</button>
+                //     <button class='placeholder'>X</button>
+                //     </div>
 
 
-                    <br> <br> <br> <br>
+                //     <br> <br> <br> <br>
                 
                 
-                ";
+                // ";
             echo "<div id='mainTable'>";
                 
                 include 'AlumniTable.php';

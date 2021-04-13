c60dcdf8a81717c0dc7283b6c0a5b9c7362d7ed9
cvancory1 <cvancory1@gmail.com>
2021-04-12 23:50:19 -0400

    testing functionality on the server

--- Homepage/chart.php
+++ Homepage/chart.php
@@ -73,7 +73,7 @@
         var string = "{'cols': [  {'id':'','label':'Topping','pattern':'','type':'string'}, {'id':'','label':'Slices','pattern':'','type':'number'}],'rows': [{'c':[{'v':'Mushrooms','f':null},{'v':3,'f':null}]},{'c':[{'v':'Onions','f':null},{'v':1,'f':null}]},{'c':[{'v':'Olives','f':null},{'v':1,'f':null}]},{'c':[{'v':'Zucchini','f':null},{'v':1,'f':null}]},{'c':[{'v':'Pepperoni','f':null},{'v':2,'f':null}]} }";
 
 
-        var data = new google.visualization.DataTable(jsonData);
+        var data = new google.visualization.DataTable(string);
 
         var options = {
           title: 'My Daily Activities',

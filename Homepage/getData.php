4078474e53dd6a879070688841f9b4e8b22d26b2
cvancory1 <cvancory1@gmail.com>
2021-04-12 22:23:22 -0400

    here

---
+++ Homepage/getData.php
@@ -0,0 +1,27 @@
+<?php 
+
+        if($connection=@mysqli_connect('localhost', 'wlucas1', 'wlucas1', 'AlumniDB')){
+            ;
+        }
+        else{
+            print '<p>ERROR: connecting to MySQL.</p>';
+        }
+
+        $query = "select programName from Program where schoolName = 'Hensen'";
+        $r=mysqli_query($connection, $query);
+        echo "here";
+
+        $json_array = array();  
+        while($row = mysqli_fetch_assoc($r))  
+        {  
+            $json_array[] = $row;  
+            
+
+        }  
+
+        $temp= json_encode($json_array);
+        echo $temp;
+        
+        mysqli_close($connection);
+
+?>

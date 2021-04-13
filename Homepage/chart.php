eacdc3fe5581c45ab4e56c0a4251922c2475a467
cvancory1 <cvancory1@gmail.com>
2021-04-12 23:52:02 -0400

    testing functionality on the server

--- Homepage/chart.php
+++ Homepage/chart.php
@@ -70,8 +70,7 @@
         // document.write(jsonData);
         // document.write("[['Program Name', 'Number'],['Biology', 5],['Chemistry ', 5],['Computer Science ', 5],['Data Science ', 5]]");
 
-        var string = "{'cols': [  {'id':'','label':'Topping','pattern':'','type':'string'}, {'id':'','label':'Slices','pattern':'','type':'number'}],'rows': [{'c':[{'v':'Mushrooms','f':null},{'v':3,'f':null}]},{'c':[{'v':'Onions','f':null},{'v':1,'f':null}]},{'c':[{'v':'Olives','f':null},{'v':1,'f':null}]},{'c':[{'v':'Zucchini','f':null},{'v':1,'f':null}]},{'c':[{'v':'Pepperoni','f':null},{'v':2,'f':null}]} }";
-
+        // var string = "{'cols': [  {'id':'','label':'Topping','pattern':'','type':'string'}, {'id':'','label':'Slices','pattern':'','type':'number'}],'rows': [{'c':[{'v':'Mushrooms','f':null},{'v':3,'f':null}]},{'c':[{'v':'Onions','f':null},{'v':1,'f':null}]},{'c':[{'v':'Olives','f':null},{'v':1,'f':null}]},{'c':[{'v':'Zucchini','f':null},{'v':1,'f':null}]},{'c':[{'v':'Pepperoni','f':null},{'v':2,'f':null}]} }";
 
         var data = new google.visualization.DataTable(string);
 
--- Homepage/pie.php
+++ Homepage/pie.php
@@ -1,30 +1,38 @@
 <html>
   <head>
+    <!--Load the AJAX API-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
     <script type="text/javascript">
-      google.charts.load("current", {packages:["corechart"]});
-      google.charts.setOnLoadCallback(drawChart);
-      function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Task', 'Hours per Day'],
-          ['Work',     11],
-          ['Eat',      2],
-          ['Commute',  2],
-          ['Watch TV', 2],
-          ['Sleep',    7]
-        ]);
-        
-        var options = {
-          title: 'My Daily Activities',
-          pieHole: 0.4,
-        };
+    
+    // Load the Visualization API and the piechart package.
+    google.charts.load('current', {'packages':['corechart']});
+      
+    // Set a callback to run when the Google Visualization API is loaded.
+    google.charts.setOnLoadCallback(drawChart);
+      
+    function drawChart() {
+    //   var jsonData = $.ajax({
+    //       url: "getData.php",
+    //       dataType: "json",
+    //       async: false
+    //       }).responseText;
+          
+      // Create our data table out of JSON data loaded from server.
+        var string = "{'cols': [  {'id':'','label':'Topping','pattern':'','type':'string'}, {'id':'','label':'Slices','pattern':'','type':'number'}],'rows': [{'c':[{'v':'Mushrooms','f':null},{'v':3,'f':null}]},{'c':[{'v':'Onions','f':null},{'v':1,'f':null}]},{'c':[{'v':'Olives','f':null},{'v':1,'f':null}]},{'c':[{'v':'Zucchini','f':null},{'v':1,'f':null}]},{'c':[{'v':'Pepperoni','f':null},{'v':2,'f':null}]} }";
 
-        var chart = new google.visualization.PieChart(document.getElementById('donutchart'));
-        chart.draw(data, options);
-      }
+      var data = new google.visualization.DataTable(string);
+
+      // Instantiate and draw our chart, passing in some options.
+      var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+      chart.draw(data, {width: 400, height: 240});
+    }
+
     </script>
   </head>
+
   <body>
-    <div id="donutchart" style="width: 900px; height: 500px;"></div>
+    <!--Div that will hold the pie chart-->
+    <div id="chart_div"></div>
   </body>
 </html>

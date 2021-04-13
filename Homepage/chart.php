33aade9acd5958f006017978075b8d03abda6614
cvancory1 <cvancory1@gmail.com>
2021-04-12 23:27:21 -0400

    testing functionality on the server

--- Homepage/chart.php
+++ Homepage/chart.php
@@ -67,11 +67,12 @@
           dataType: "json",
           async: false
           }).responseText;
-        // document.write(jsonData);
-        // document.write("[['Program Name', 'Number'],['Biology', 5],['Chemistry ', 5],['Computer Science ', 5],['Data Science ', 5]]");
+        document.write(jsonData);
+        document.write("\n");
+        document.write("[['Program Name', 'Number'],['Biology', 5],['Chemistry ', 5],['Computer Science ', 5],['Data Science ', 5]]");
 
-        var data = google.visualization.arrayToDataTable([['Program Name', 'Number'],['Biology', 5],['Chemistry ', 5],['Computer Science ', 5],['Data Science ', 5]]);
-        // var data = google.visualization.arrayToDataTable(jsonData);
+        // var data = google.visualization.arrayToDataTable([['Program Name', 'Number'],['Biology', 5],['Chemistry ', 5],['Computer Science ', 5],['Data Science ', 5]]);
+        var data = google.visualization.arrayToDataTable(jsonData);
 
         var options = {
           title: 'My Daily Activities',

7795242bde2632045bd91a006afb740cfdb60322
cvancory1 <cvancory1@gmail.com>
2021-04-15 19:45:45 -0400

    testing functionality on the server

--- Homepage/barchart.php
+++ Homepage/barchart.php
@@ -7,22 +7,22 @@
       google.charts.setOnLoadCallback(drawChart);
 
       function drawChart() {
-        var data = google.visualization.arrayToDataTable([
-          ['Year', 'Sales', 'Expenses', 'Profit'],
-          ['2014', 1000, 400, 200],
-          ['2015', 1170, 460, 250],
-          ['2016', 660, 1120, 300],
-          ['2017', 1030, 540, 350]
-        ]);
+        // var data = google.visualization.arrayToDataTable([
+        //   ['Year', 'Sales', 'Expenses', 'Profit'],
+        //   ['2014', 1000, 400, 200],
+        //   ['2015', 1170, 460, 250],
+        //   ['2016', 660, 1120, 300],
+        //   ['2017', 1030, 540, 350]
+        // ]);
 
         // alert(data);
         console.log(data);
 
-        // var jsonData = $.ajax({
-        //   url: "getDonations.php",
-        //   dataType: "json",
-        //   async: false
-        //   }).responseText;
+        var jsonData = $.ajax({
+          url: "getDonations.php",
+          dataType: "json",
+          async: false
+          }).responseText;
 
 
         var options = {
--- Homepage/getDonations.php
+++ Homepage/getDonations.php
@@ -10,28 +10,33 @@
         $query = "select schoolName, sum(amount) from Donated natural join (select distinct alumniID, schoolName from MajoredIn natural join Program)Major group by schoolName;";
         $r=mysqli_query($connection, $query);
 
-//f
-        echo "{\"cols\": [{\"id\":\"\",\"label\":\"School Name\",\"pattern\":\"\",\"type\":\"string\"}, {\"id\":\"\",\"label\":\"Dollars Donated\",\"pattern\":\"\",\"type\":\"number\"}],\"rows\": [";
+// //f
+//         echo "{\"cols\": [{\"id\":\"\",\"label\":\"School Name\",\"pattern\":\"\",\"type\":\"string\"}, {\"id\":\"\",\"label\":\"Dollars Donated\",\"pattern\":\"\",\"type\":\"number\"}],\"rows\": [";
      
             
-        $amount = mysqli_num_rows($r);
-        for( $i=0; $i< $amount-1 ; $i++){
-            $row = mysqli_fetch_assoc($r);
+//         $amount = mysqli_num_rows($r);
+//         for( $i=0; $i< $amount-1 ; $i++){
+//             $row = mysqli_fetch_assoc($r);
 
-            echo "{\"c\":[{\"v\":\"";
-            echo $row['schoolName'];
-            echo "\",\"f\":null},{\"v\":";
-            echo $row['sum(amount)'];
-            echo",\"f\":null}]},";
+//             echo "{\"c\":[{\"v\":\"";
+//             echo $row['schoolName'];
+//             echo "\",\"f\":null},{\"v\":";
+//             echo $row['sum(amount)'];
+//             echo",\"f\":null}]},";
 
-        }
-//
-        $row = mysqli_fetch_assoc($r);
-        echo "{\"c\":[{\"v\":\"";
-        echo $row['schoolName'];
-        echo "\",\"f\":null},{\"v\":";
-        echo $row['sum(amount)'];
-        echo ",\"f\":null}]}] }";
+//         }
+// //
+//         $row = mysqli_fetch_assoc($r);
+//         echo "{\"c\":[{\"v\":\"";
+//         echo $row['schoolName'];
+//         echo "\",\"f\":null},{\"v\":";
+//         echo $row['sum(amount)'];
+//         echo ",\"f\":null}]}] }";
+
+echo "{c: [{v: '2015'}, {v: 1170}, {v: 460}, {v: 250}], p: undefined}";
+echo "{c: [{v: '2015'}, {v: 1170}, {v: 460}, {v: 250}], p: undefined}";
+echo "{c: [{v: '2015'}, {v: 1170}, {v: 460}, {v: 250}], p: undefined}";
+echo "{c: [{v: '2015'}, {v: 1170}, {v: 460}, {v: 250}], p: undefined}";
 
 
        

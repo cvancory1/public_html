797db1410c017ae6502b2ee4f4dd4e111b6b7711
cvancory1 <cvancory1@gmail.com>
2021-04-15 19:17:59 -0400

    l

---
+++ Homepage/getDonations.php
@@ -0,0 +1,39 @@
+<?php 
+
+        if($connection=@mysqli_connect('localhost', 'wlucas1', 'wlucas1', 'AlumniDB')){
+            ;
+        }
+        else{
+            print '<p>ERROR: connecting to MySQL.</p>';
+        }
+
+        $query = "select schoolName, sum(amount) from Donated natural join (select distinct alumniID, schoolName from MajoredIn natural join Program)Major group by schoolName;";
+        $r=mysqli_query($connection, $query);
+
+
+        echo "{\"cols\": [{\"id\":\"\",\"label\":\"Program Name\",\"pattern\":\"\",\"type\":\"string\"}, {\"id\":\"\",\"label\":\"Major\",\"pattern\":\"\",\"type\":\"number\"}],\"rows\": [";
+     
+            
+        $amount = mysqli_num_rows($r);
+        for( $i=0; $i< $amount-1 ; $i++){
+            $row = mysqli_fetch_assoc($r);
+
+            echo "{\"c\":[{\"v\":\"";
+            echo $row['schoolName'];
+            echo "\",\"f\":null},{\"v\":";
+            echo $row['sum(amount)'];
+            echo",\"f\":null}]},";
+
+        }
+//
+        $row = mysqli_fetch_assoc($r);
+        echo "{\"c\":[{\"v\":\"";
+        echo $row['programName'];
+        echo "\",\"f\":null},{\"v\":";
+        echo $row['sum(amount)'];
+        echo ",\"f\":null}]}] }";
+
+
+       
+         mysqli_close($connection);
+?>

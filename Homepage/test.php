b8305735252ab500f2082ed61631ed87daad7987
cvancory1 <cvancory1@gmail.com>
2021-04-16 19:34:41 -0400

    testing functionality on the server

--- Homepage/test.php
+++ Homepage/test.php
@@ -6,6 +6,7 @@
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
         <link rel="stylesheet" href="dat.css" type="text/css" >
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     </head>
 
 

af58199c031d36827d58baf1d579092b71e61942
cvancory1 <cvancory1@gmail.com>
2021-04-15 20:40:41 -0400

    testing functionality on the server

--- Homepage/delete.php
+++ Homepage/delete.php
@@ -20,38 +20,38 @@
     // echo $_POST['delete[]'] ;
 
 
-    if(isset($_POST['submit'])){
-        echo "here1";
+    // if(isset($_POST['submit'])){
+    //     echo "here1";
 
-        if(!empty($_POST['delete'])) {
-            echo "here2";
-            echo $_POST['delete'];
+    //     if(!empty($_POST['delete'])) {
+    //         echo "here2";
+    //         echo $_POST['delete'];
             
-            foreach($_POST['delete'] as $value){
-                echo "value : ".$value.'<br/>';
-            }
+    //         foreach($_POST['delete'] as $value){
+    //             echo "value : ".$value.'<br/>';
+    //         }
     
-        }
+    //     }
     
-    }
+    // }
 
     // // if the form deleteRows was submitted 
-	// if (isset($_POST['submit']) && isset($_POST['delete[]'])){
+	if (isset($_POST['submit']) && isset($_POST['delete[]'])){
 
-    // echo "here2";
+    echo "here2";
    
 
-	// 	foreach($_POST['delete'] as $id)
-	// 	{
-	// 		$id = (int)$id;
-    //         echo $id;
-	// 		$sql = "DELETE FROM Alumni WHERE alumniID = $id";
-    //         echo $sql;
+		foreach($_POST['delete'] as $id)
+		{
+			$id = (int)$id;
+            echo $id;
+			$sql = "DELETE FROM Alumni WHERE alumniID = $id";
+            echo $sql;
 
-    //         mysqli_query($connection, $sql);
+            mysqli_query($connection, $sql);
 
-	// 	}
-	// }
+		}
+	}
     mysqli_close($connection);
 
 ?>

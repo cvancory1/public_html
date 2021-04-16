36f8e33defe70f2186bae56f34baf0e888be36ed
cvancory1 <cvancory1@gmail.com>
2021-04-15 21:40:09 -0400

    testing functionality on the server

--- Homepage/delete.php
+++ Homepage/delete.php
@@ -23,52 +23,7 @@
        
       }
 
-    // $number = count ($_POST)["delete"];
-    // echo "$number";
-
-    // echo "here";
-    // // echo $_POST['checkbox'] ;
-    // // echo $_POST['delete'];
-    // // echo $_POST['delete'][0];
-
-    // echo $_POST['delete[]'] ;
-
-    // echo $_POST['submit'];
-
-
-    // if(isset($_POST['submit'])){
-    //     echo "here1";
-
-    //     if(!empty($_POST['delete'])) {
-    //         echo "here2";
-    //         echo $_POST['delete'];
-            
-    //         foreach($_POST['delete'] as $value){
-    //             echo "value : ".$value.'<br/>';
-    //         }
-    
-    //     }
-    
-    // }
-
-    // // // if the form deleteRows was submitted  a
-	// if (isset($_POST['submit']) && isset($_POST['delete[]'])){
-
-    // echo "here2";
-   
-
-	// 	foreach($_POST['delete'] as $id)
-	// 	{
-	// 		$id = (int)$id;
-    //         echo $id;
-	// 		$sql = "DELETE FROM Alumni WHERE alumniID = $id";
-    //         echo $sql;
-
-    //         mysqli_query($connection, $sql);
-
-	// 	}
-	// }
-
     mysqli_close($connection);
+    header( "refresh:1;url=https://lamp.salisbury.edu/~cvancory1/Homepage/deletetesting.php" );
 
 ?>

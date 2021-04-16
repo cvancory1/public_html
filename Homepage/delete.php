7be583f3194050c6fe6e94640790b1a51328e636
cvancory1 <cvancory1@gmail.com>
2021-04-15 22:13:31 -0400

    testing functionality on the server

--- Homepage/delete.php
+++ Homepage/delete.php
@@ -23,7 +23,7 @@
         // }
     // }
 
-        if(isset($_POST["submit"]) && $_POST["submit"]!="") {
+        if(isset($_POST["submit"]) ) {
             $usersCount = count($_POST["delete"]);
             echo $usersCount;
             for($i=0;$i<$usersCount;$i++) {

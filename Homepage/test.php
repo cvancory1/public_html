a2852329d6b4503e7bed6c68b22f6dbac21e82ef
cvancory1 <cvancory1@gmail.com>
2021-04-19 17:59:29 -0400

    testing functionality on the server

--- Homepage/test.php
+++ Homepage/test.php
@@ -123,53 +123,111 @@ $_SESSION['LAST_ACTIVITY'] = $time;
                 </div>
                 <br> <br> <br> <br>";
         ?>
-                <div align="center" >
+              <div id="main">
+            <script type="text/javascript">
+                function filterTable(ele) {
+                    var id = ele.id;
+                    let input = document.getElementById(id);
+                    let filter = input.value.toUpperCase();
+                    let table = document.getElementById("alumniTable");
+                    let tr = table.getElementsByTagName("tr");
+
+                    var index;
+                    if (id == "alumniIDInput"){
+                        index = 0;
+                    }
+                    else if (id == "birthdateInput"){
+                        index = 1;
+                    }
+                    else if (id == "statusInput"){
+                        index = 2;
+                    }
+                    else if (id == "emailInput"){
+                        index = 3;
+                    }
+                    else if (id == "phoneNumberInput"){
+                        index = 4;
+                    }
+                    else if (id == "firstNameInput"){
+                        index = 5;
+                    }
+                    else if (id == "middleNameInput"){
+                        index = 6;
+                    }
+                    else if (id == "lastNameInput"){
+                        index = 7;
+                    }
+                    else if (id == "streetNameInput"){
+                        index = 8;
+                    }
+                    else if (id == "cityInput"){
+                        index = 9;
+                    }
+                    else if (id == "stateInput"){
+                        index = 10;
+                    }
+                    else if (id == "countryRegionInput"){
+                        index = 11;
+                    }
+                    else if (id == "zipcodeInput"){
+                        index = 12;
+                    }
+
+                    for (let i = 0; i < tr.length; i++) {
+                        td = tr[i].getElementsByTagName("td")[index];
+                        if (td) {
+                            let txt = td.textContent || td.innerText;
+                            if (txt.toUpperCase().indexOf(filter) > -1) {
+                                tr[i].style.display = "";
+                            }
+                            else {
+                                tr[i].style.display = "none";
+                            }
+                        }
+                    }
+                }
+            </script>
+
+            <div align="center">
                 <table>
-                <tr> 
-                <td>
-
-
-                <select class="inputTable" data-target=".tableSelect"  name="inputTable">
-                    <option value="alumniID" data-show=".alumniID"  selected>Alumni ID</option>
-                    <option value="birthdate" data-show=".birthdate">Birthdate</option>
-                    <option value="status" data-show=".status">Status</option>
-                    <option value="email" data-show=".email">Email</option>
-                    <option value="phoneNumber" data-show=".phoneNumber">Phone Number</option>
-                    <option value="firstName" data-show=".firstName">First Name</option>
-                    <option value="middleName" data-show=".middleName">Middle Name</option>
-                    <option value="lastName" data-show=".lastName">Last Name</option>
-                    <option value="streetName" data-show=".streetName">Street Name</option>
-                    <option value="city" data-show=".city">City</option>
-                    <option value="state" data-show=".state">State</option>
-                    <option value="countryRegion" data-show=".countryRegion">Country/Region</option>
-                    <option value="zipcode" data-show=".zipcode">Zipcode</option>
-                </select>
-
-                </td>
-                <td>
-                
-                <!-- search bar -->
-                <div class="tableSelect" id = tableTest>
-                    <div class="alumniID hide"><input type="text" id="alumniIDInput" class="box" onkeyup="filterTable(this);" name="alumniID" placeholder="Search for Alumni ID..."></div>
-                    <div class="birthdate hide"><input type="text" id="birthdateInput" class="box" onkeyup="filterTable(this);" name="birthdate" placeholder="Search for Birthdate..."></div>
-                    <div class="status hide"><input type="text" id="statusInput" class="box" onkeyup="filterTable(this);" name="status" placeholder="Search for Status..."></div>
-                    <div class="email hide"><input type="text" id="emailInput" class="box" onkeyup="filterTable(this);" name="email" placeholder="Search for Email..."></div>
-                    <div class="phoneNumber hide"><input type="text" id="phoneNumberInput" class="box" onkeyup="filterTable(this);" name="phoneNumber" placeholder="Search for Phone Number..."></div>
-                    <div class="firstName hide"><input type="text" id="firstNameInput" class="box" onkeyup="filterTable(this);" name="firstName" placeholder="Search for First Name..."></div>
-                    <div class="middleName hide"><input type="text" id="middleNameInput" class="box" onkeyup="filterTable(this);" name="middleName" placeholder="Search for Middle Name..."></div>
-                    <div class="lastName hide"><input type="text" id="lastNameInput" class="box" onkeyup="filterTable(this);" name="lastName" placeholder="Search for Last Name..."></div>
-                    <div class="streetName hide"><input type="text" id="streetNameInput" class="box" onkeyup="filterTable(this);" name="streetName" placeholder="Search for Street Name..."></div>
-                    <div class="city hide"><input type="text" id="cityInput" class="box" onkeyup="filterTable(this);" name="city" placeholder="Search for City..."></div>
-                    <div class="state hide"><input type="text" id="stateInput" class="box" onkeyup="filterTable(this);" name="state" placeholder="Search for State..."></div>
-                    <div class="countryRegion hide"><input type="text" id="countryRegionInput" class="box" onkeyup="filterTable(this);" name="countryRegion" placeholder="Search for Country/Region..."></div>
-                    <div class="zipcode hide"><input type="text" id="zipcodeInput" class="box" onkeyup="filterTable(this);" name="zipcode" placeholder="Search for Zipcode..."></div>
-                </div>
-
-                </td>
-                </tr>
+                    <tr>
+                        <td>
+                            <select class="inputTable" data-target=".tableSelect"  name="inputTable">
+                                <option value="alumniID" data-show=".alumniID" selected>Alumni ID</option>
+                                <option value="birthdate" data-show=".birthdate">Birthdate</option>
+                                <option value="status" data-show=".status">Status</option>
+                                <option value="email" data-show=".email">Email</option>
+                                <option value="phoneNumber" data-show=".phoneNumber">Phone Number</option>
+                                <option value="firstName" data-show=".firstName">First Name</option>
+                                <option value="middleName" data-show=".middleName">Middle Name</option>
+                                <option value="lastName" data-show=".lastName">Last Name</option>
+                                <option value="streetName" data-show=".streetName">Street Name</option>
+                                <option value="city" data-show=".city">City</option>
+                                <option value="state" data-show=".state">State</option>
+                                <option value="countryRegion" data-show=".countryRegion">Country/Region</option>
+                                <option value="zipcode" data-show=".zipcode">Zipcode</option>
+                            </select>
+                        </td>
+                        
+                        <td>
+                            <div class="tableSelect">
+                                <div class="alumniID hide"><input type="text" id="alumniIDInput" class="box" onkeyup="filterTable(this);" name="alumniID" placeholder="Search for Alumni ID..."></div>
+                                <div class="birthdate hide"><input type="text" id="birthdateInput" class="box" onkeyup="filterTable(this);" name="birthdate" placeholder="Search for Birthdate..."></div>
+                                <div class="status hide"><input type="text" id="statusInput" class="box" onkeyup="filterTable(this);" name="status" placeholder="Search for Status..."></div>
+                                <div class="email hide"><input type="text" id="emailInput" class="box" onkeyup="filterTable(this);" name="email" placeholder="Search for Email..."></div>
+                                <div class="phoneNumber hide"><input type="text" id="phoneNumberInput" class="box" onkeyup="filterTable(this);" name="phoneNumber" placeholder="Search for Phone Number..."></div>
+                                <div class="firstName hide"><input type="text" id="firstNameInput" class="box" onkeyup="filterTable(this);" name="firstName" placeholder="Search for First Name..."></div>
+                                <div class="middleName hide"><input type="text" id="middleNameInput" class="box" onkeyup="filterTable(this);" name="middleName" placeholder="Search for Middle Name..."></div>
+                                <div class="lastName hide"><input type="text" id="lastNameInput" class="box" onkeyup="filterTable(this);" name="lastName" placeholder="Search for Last Name..."></div>
+                                <div class="streetName hide"><input type="text" id="streetNameInput" class="box" onkeyup="filterTable(this);" name="streetName" placeholder="Search for Street Name..."></div>
+                                <div class="city hide"><input type="text" id="cityInput" class="box" onkeyup="filterTable(this);" name="city" placeholder="Search for City..."></div>
+                                <div class="state hide"><input type="text" id="stateInput" class="box" onkeyup="filterTable(this);" name="state" placeholder="Search for State..."></div>
+                                <div class="countryRegion hide"><input type="text" id="countryRegionInput" class="box" onkeyup="filterTable(this);" name="countryRegion" placeholder="Search for Country/Region..."></div>
+                                <div class="zipcode hide"><input type="text" id="zipcodeInput" class="box" onkeyup="filterTable(this);" name="zipcode" placeholder="Search for Zipcode..."></div>
+                            </div>
+                        </td>
+                    </tr>
                 </table>
-
-
             </div>
 
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -191,7 +249,11 @@ $_SESSION['LAST_ACTIVITY'] = $time;
                 });
             </script>
 
-    </div>
+            <style>
+                .hide {
+                    display: none;
+                }
+            </style>
 
     <?php
 

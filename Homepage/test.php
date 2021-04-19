9582a3a93a53c1ab02ef53669801aa4fc1585c6c
cvancory1 <cvancory1@gmail.com>
2021-04-19 17:56:03 -0400

    testing functionality on the server

--- Homepage/test.php
+++ Homepage/test.php
@@ -122,40 +122,8 @@ $_SESSION['LAST_ACTIVITY'] = $time;
         echo " <button class='placeholder' id='signout'  title='Sign Out' onclick='logout()'> <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' width='24' height='24'><path fill='none' d='M0 0h24v24H0z'/><path d='M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2a9.985 9.985 0 0 1 8 4h-2.71a8 8 0 1 0 .001 12h2.71A9.985 9.985 0 0 1 12 22zm7-6v-3h-8v-2h8V8l5 4-5 4z' fill='rgba(255,255,255,1)'/></svg> </button>
                 </div>
                 <br> <br> <br> <br>";
-
-
-
-        echo "<div id='mainTable' >";
-        include 'AlumniTable.php';
-        echo "</div>"; // end of mainTable k
-
-        echo "<div id='totalMajors' style='display:none;' >";
-        include 'pie.php';
-        echo "</div>"; // shows the pie chart
-
-
-        echo "<div id='deleteTable'  style='display:none;' >";
-        include 'deleteTable.php';
-        echo "</div>"; // end of table shown for deleting  class
-
-        echo "<div id='superView'  style='display:none;' >";
-        include 'superView.php';
-        echo "</div>"; // end of superview div
-
-    echo "</div>"; // end of webpage class
-
-
-        // }
-    }//end "if" privlege
-
-
-    
-
- 
-
-?>
-
-        <div align="center" >
+        ?>
+                <div align="center" >
                 <table>
                 <tr> 
                 <td>
@@ -224,6 +192,41 @@ $_SESSION['LAST_ACTIVITY'] = $time;
             </script>
 
     </div>
+
+    <?php
+
+
+        echo "<div id='mainTable' >";
+        include 'AlumniTable.php';
+        echo "</div>"; // end of mainTable k
+
+        echo "<div id='totalMajors' style='display:none;' >";
+        include 'pie.php';
+        echo "</div>"; // shows the pie chart
+
+
+        echo "<div id='deleteTable'  style='display:none;' >";
+        include 'deleteTable.php';
+        echo "</div>"; // end of table shown for deleting  class
+
+        echo "<div id='superView'  style='display:none;' >";
+        include 'superView.php';
+        echo "</div>"; // end of superview div
+
+    echo "</div>"; // end of webpage class
+
+
+        // }
+    }//end "if" privlege
+
+
+ 
+
+?>
+
+
+
+       
 
 <script type="text/javascript">
 

31878735072bb383bb0cefe748f372b0b3b65414
cvancory1 <cvancory1@gmail.com>
2021-04-19 20:14:26 -0400

    testing functionality on the server

--- Homepage/emailTest.php
+++ Homepage/emailTest.php
@@ -9,59 +9,59 @@
     <body>
     <?php
             // Connect to MySQL Database
-            if($connection=@mysqli_connect('localhost', 'wlucas1', 'wlucas1', 'AlumniDB')){
-                ;
-            }
-            else{
-                print '<p>ERROR: connecting to MySQL.</p>';
-            }
+            // if($connection=@mysqli_connect('localhost', 'wlucas1', 'wlucas1', 'AlumniDB')){
+            //     ;
+            // }
+            // else{
+            //     print '<p>ERROR: connecting to MySQL.</p>';
+            // }
 
-            //Query to return contents of table Alumni
-            $query="SELECT * FROM Alumni";
-            $r=mysqli_query($connection, $query);
-                echo "<table id='alumniTable' class='styled-table'>
-                    <thead>
-                        <tr>
-                            <th> Select </th>
-                            <th> Alumni ID </th>
-                            <th> Birthdate </th>
-                            <th> Status </th>
-                            <th> Email </th>
-                            <th> Phone Number </th>
-                            <th> First Name </th>
-                            <th> Middle Name </th>
-                            <th> Last Name </th>
-                            <th> Street Name </th>
-                            <th> City </th>
-                            <th> State </th>
-                            <th> Country/Region </th>
-                            <th> Zipcode </th>
-                        </tr>
-                    </thead>";
+            // //Query to return contents of table Alumni
+            // $query="SELECT * FROM Alumni";
+            // $r=mysqli_query($connection, $query);
+            //     echo "<table id='alumniTable' class='styled-table'>
+            //         <thead>
+            //             <tr>
+            //                 <th> Select </th>
+            //                 <th> Alumni ID </th>
+            //                 <th> Birthdate </th>
+            //                 <th> Status </th>
+            //                 <th> Email </th>
+            //                 <th> Phone Number </th>
+            //                 <th> First Name </th>
+            //                 <th> Middle Name </th>
+            //                 <th> Last Name </th>
+            //                 <th> Street Name </th>
+            //                 <th> City </th>
+            //                 <th> State </th>
+            //                 <th> Country/Region </th>
+            //                 <th> Zipcode </th>
+            //             </tr>
+            //         </thead>";
 
-                while($row=mysqli_fetch_array($r)){
-                    echo "<tr>";
-                    $email = $row['email'];
-                    echo "<td><input type='checkbox' value='$email'></td>";
-                    echo "<td>" . $row['alumniID'] . "</td>";
-                    echo "<td>" . $row['birthdate'] . "</td>";
-                    echo "<td>" . $row['status'] . "</td>";
-                    echo "<td>" . $row['email'] . "</td>";
-                    echo "<td>" . $row['phoneNumber'] . "</td>";
-                    echo "<td>" . $row['firstName'] . "</td>";
-                    echo "<td>" . $row['middleName'] . "</td>";
-                    echo "<td>" . $row['lastName'] . "</td>";
-                    echo "<td>" . $row['streetName'] . "</td>";
-                    echo "<td>" . $row['city'] . "</td>";
-                    echo "<td>" . $row['state'] . "</td>";
-                    echo "<td>" . $row['countryRegion'] . "</td>";
-                    echo "<td>" . $row['zipcode'] . "</td>";
-                    echo "</tr>";
-                }
+            //     while($row=mysqli_fetch_array($r)){
+            //         echo "<tr>";
+            //         $email = $row['email'];
+            //         echo "<td><input type='checkbox' value='$email'></td>";
+            //         echo "<td>" . $row['alumniID'] . "</td>";
+            //         echo "<td>" . $row['birthdate'] . "</td>";
+            //         echo "<td>" . $row['status'] . "</td>";
+            //         echo "<td>" . $row['email'] . "</td>";
+            //         echo "<td>" . $row['phoneNumber'] . "</td>";
+            //         echo "<td>" . $row['firstName'] . "</td>";
+            //         echo "<td>" . $row['middleName'] . "</td>";
+            //         echo "<td>" . $row['lastName'] . "</td>";
+            //         echo "<td>" . $row['streetName'] . "</td>";
+            //         echo "<td>" . $row['city'] . "</td>";
+            //         echo "<td>" . $row['state'] . "</td>";
+            //         echo "<td>" . $row['countryRegion'] . "</td>";
+            //         echo "<td>" . $row['zipcode'] . "</td>";
+            //         echo "</tr>";
+            //     }
 
-                echo "</table>";
+            //     echo "</table>";
 
-            mysqli_close($connection);
+            // mysqli_close($connection);
         ?>
 
     <form action="test.php" method="post">

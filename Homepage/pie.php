8bfdd54613850f693bbb95d7cd76b7e5b6e2d3b4
cvancory1 <cvancory1@gmail.com>
2021-04-19 22:37:47 -0400

    testing functionality on the server

--- Homepage/pie.php
+++ Homepage/pie.php
@@ -26,7 +26,7 @@
 
       // Instantiate and draw our chart, passing in some options.
       var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-      chart.draw(data, {width: 800, height: 640});
+      chart.draw(data, {width: 800, height: 690});
     }
 
     </script>

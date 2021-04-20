66adb993e8315b70173445117c3e2ede4ff5ab7a
cvancory1 <cvancory1@gmail.com>
2021-04-20 10:20:58 -0400

    testing functionality on the server

--- Homepage/pie.php
+++ Homepage/pie.php
@@ -36,6 +36,15 @@
 
   <body>
     <!--Div that will hold the pie chart-->
+  
+
+
+
+
+    <div style='width: 500px; height: 300px; margin-left: auto; margin-right: auto;'>
+        <style>.embed-container {position: relative; padding-bottom: 80%; height: 0; max-width: 100%;} .embed-container iframe, .embed-container object, .embed-container iframe{position: absolute; top: 0; left: 0; width: 100%; height: 100%;} small{position: absolute; z-index: 40; bottom: 0; margin-bottom: -15px;}</style><div class="embed-container"><small><a href="//salisburyu.maps.arcgis.com/apps/Embed/index.html?webmap=d23174a625144d8c8f9cdd31fe766b4a&extent=-84.7735,32.9276,-66.723,41.3703&home=true&zoom=true&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=legend&disable_scroll=false&theme=light" style="color:#0000FF;text-align:left" target="_blank">View larger map</a></small><br><iframe width="500" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" title="Alumni DB Demo" src="//salisburyu.maps.arcgis.com/apps/Embed/index.html?webmap=d23174a625144d8c8f9cdd31fe766b4a&extent=-84.7735,32.9276,-66.723,41.3703&home=true&zoom=true&previewImage=false&scale=true&search=true&searchextent=true&details=true&legendlayers=true&active_panel=legend&disable_scroll=false&theme=light"></iframe></div>
+    </div>
     <div id="chart_div"></div>
+
   </body>
 </html>

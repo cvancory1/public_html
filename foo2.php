f4897f5a202c58fcc106377c2c750c2e29a99f6e
cvancory1 <cvancory1@gmail.com>
2021-04-22 14:58:51 -0400

    testing functionality on the server

--- foo2.php
+++ foo2.php
@@ -11,16 +11,16 @@
         if(isset($_POST["but_delete"]) ) {
             $usersCount = count($_POST["delete"]);
             echo $usersCount;
-        //     for($i=0;$i<$usersCount;$i++) {
-        //         $delete = $_POST['delete'][$i];
-        //         $birthdate = $_POST['birthday'][$i];
-        //         echo $delete . " ". $birthdate . " ";
-        //         $sql = " DELETE FROM Alumni WHERE alumniID =$delete  AND birthdate = '$birthdate' ";
-        //         echo $sql;
-        //         $resuli = mysqli_query($connection, $sql);
+            for($i=0;$i<$usersCount;$i++) {
+                $delete = $_POST['delete'][$i];
+                $birthdate = $_POST['birthday'][$i];
+                echo $delete . " ". $birthdate . " ";
+                $sql = " DELETE FROM Alumni WHERE alumniID =$delete  AND birthdate = '$birthdate' ";
+                echo $sql;
+                $resuli = mysqli_query($connection, $sql);
 
                 
-            // }
+            }
         }
     mysqli_close($connection);
     // header( "refresh:5;url=https://lamp.salisbury.edu/~cvancory1/Homepage/test.php" );

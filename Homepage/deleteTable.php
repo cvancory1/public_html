c7de3ed6590a5edae1c850d2b36c7cd5016475d1
cvancory1 <cvancory1@gmail.com>
2021-04-22 14:45:46 -0400

    testing functionality on the server

--- Homepage/deleteTable.php
+++ Homepage/deleteTable.php
@@ -1,24 +1,84 @@
-<!-- Holds the code which displays the username and privelege of all users in the database 
-    - calls updatePrivelege.php if the superuser wants to update priveleges
+<!-- Holds the code which displays the Alumni table in the database 
+    - calls deleteTable.php if the superuser wants to delete Alumni 
     - is #included in other files  -->
 
-    <!DOCTYPE html>
+    <html>
+<!DOCTYPE html>
 <html lang = "en">
-
-    <header>
+<header>
         <title> Alumni Database</title>
         <meta name="viewport" content="width=device-width, initial-scale=1"> 
         <link rel="stylesheet" href="dat.css" type="text/css" />
     </header>
 
+ <form method='post' action ='delete.php'>
+    <input type='submit' class= 'submitButton'  name='but_delete'><br><br>
+    <!-- <input type='submit'  value='delete' name='but_delete'><br><br> -->
 
-    <form method='post' action='updatePrivilege.php'>
-    <input type='submit'class= 'submitButton'  value='Update' name='submit_button'>
+<?php
 
-      
+        if($connection = @mysqli_connect('localhost', 'wlucas1', 'wlucas1', 'AlumniDB')){
+                ;
+            }
+            else{
+                print '<p>ERROR: connecting to MySQL.</p>';
+            }
+
+            // echo"<form name='deleteRows' id='deleteRows' method='post'  action='delete.php' "; 
+
+                //Query to return contents of table Alumni here 
+                $query="SELECT * FROM Alumni";
+                $r=mysqli_query($connection, $query);
+                echo "<table id='alumniDelete' class='styled-table'>
+                    <thead>
+                        <tr>
+                        <th> Select </th>
+                        <th> Alumni ID </th>
+                        <th> Birthdate </th>
+                        <th> Status </th>
+                        <th> Email </th>
+                        <th> Phone Number </th>
+                        <th> First Name </th>
+                        <th> Middle Name </th>
+                        <th> Last Name </th>
+                        <th> Street Name </th>
+                        <th> City </th>
+                        <th> State </th>
+                        <th> Country/Region </th>
+                        <th> Zipcode </th>
+                    </tr>
+                </thead>";
+
+            while($row=mysqli_fetch_array($r)){
+                echo "<tr>";
+                $id = $row['alumniID'];
+                $bday = $row['birthdate'];
+                echo "<td><input type='checkbox' name='delete[]' value='$id' ></td>";
+                echo "<input type='hidden' name='birthday[]' value= $bday >";
+ 
+                echo "<td>" . $row['alumniID'] . "</td>";
+                echo "<td>" . $row['birthdate'] . "</td>";
+                echo "<td>" . $row['status'] . "</td>";
+                echo "<td>" . $row['email'] . "</td>";
+                echo "<td>" . $row['phoneNumber'] . "</td>";
+                echo "<td>" . $row['firstName'] . "</td>";
+                echo "<td>" . $row['middleName'] . "</td>";
+                echo "<td>" . $row['lastName'] . "</td>";
+                echo "<td>" . $row['streetName'] . "</td>";
+                echo "<td>" . $row['city'] . "</td>";
+                echo "<td>" . $row['state'] . "</td>";
+                echo "<td>" . $row['countryRegion'] . "</td>";
+                echo "<td>" . $row['zipcode'] . "</td>";
+                echo "</tr>";
+            }
+
+            echo "</table>";
+
+            mysqli_close($connection);
+
+?>
+
 </form>
-<!-- as -->
-
-
 
 </html>
+

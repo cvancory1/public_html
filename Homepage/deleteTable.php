78ff9633b65674dd8bb2d0462a0c5d3316a719e9
cvancory1 <cvancory1@gmail.com>
2021-04-22 14:55:05 -0400

    testing functionality on the server

--- Homepage/deleteTable.php
+++ Homepage/deleteTable.php
@@ -1,16 +1,21 @@
 <!-- Holds the code which displays the Alumni table in the database 
     - calls deleteTable.php if the superuser wants to delete Alumni 
     - is #included in other files  -->
+
     <html>
 <!DOCTYPE html>
 <html lang = "en">
+<header>
+        <title> Alumni Database</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <link rel="stylesheet" href="dat.css" type="text/css" />
+    </header>
 
+ <form method='post' action ='delete.php'>
+    <input type='submit' class= 'submitButton'  name='but_delete'><br><br>
+    <!-- <input type='submit'  value='delete' name='but_delete'><br><br> -->
 
-  <form method='post' action ='delete.php'>
-     <!-- <input type='submit' class= 'submitButton' value='Delete' name='but_delete'><br><br> -->
-     <input type='submit'  value='delete' name='but_delete'><br><br>
-
- <?php
+<?php
 
         if($connection = @mysqli_connect('localhost', 'wlucas1', 'wlucas1', 'AlumniDB')){
                 ;
@@ -18,7 +23,9 @@
             else{
                 print '<p>ERROR: connecting to MySQL.</p>';
             }
+
             // echo"<form name='deleteRows' id='deleteRows' method='post'  action='delete.php' "; 
+
                 //Query to return contents of table Alumni here 
                 $query="SELECT * FROM Alumni";
                 $r=mysqli_query($connection, $query);
@@ -41,6 +48,7 @@
                         <th> Zipcode </th>
                     </tr>
                 </thead>";
+
             while($row=mysqli_fetch_array($r)){
                 echo "<tr>";
                 $id = $row['alumniID'];
@@ -63,8 +71,14 @@
                 echo "<td>" . $row['zipcode'] . "</td>";
                 echo "</tr>";
             }
+
             echo "</table>";
+
             mysqli_close($connection);
+
 ?>
+
 </form>
+
 </html>
+
---
+++ foo2.php
@@ -0,0 +1,28 @@
+<?php
+    echo "here";
+    if($connection=@mysqli_connect('localhost', 'wlucas1', 'wlucas1', 'AlumniDB')){
+        ;
+    }
+    else{
+        print '<p>ERROR: connecting to MySQL.</p>';
+    }
+
+   
+        // if(isset($_POST["but_delete"]) ) {
+        //     $usersCount = count($_POST["delete"]);
+        //     echo $usersCount;
+        //     for($i=0;$i<$usersCount;$i++) {
+        //         $delete = $_POST['delete'][$i];
+        //         $birthdate = $_POST['birthday'][$i];
+        //         echo $delete . " ". $birthdate . " ";
+        //         $sql = " DELETE FROM Alumni WHERE alumniID =$delete  AND birthdate = '$birthdate' ";
+        //         echo $sql;
+        //         $resuli = mysqli_query($connection, $sql);
+
+                
+        //     }
+        // }
+    mysqli_close($connection);
+    // header( "refresh:5;url=https://lamp.salisbury.edu/~cvancory1/Homepage/test.php" );
+
+?>
